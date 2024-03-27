cd5a3d12e2aee6cfe01dae8c415d9007c1fe38c4
caleb_liu <xiaoweiba1028@gmail.com>
2024-03-27 12:51:16 +0800

    完成getAccessToken

--- Baidu-Web-Api/src/main/java/pp/weiba/thirdparty/baidu/web/api/security/authentication/AuthenticationApiClient.java
+++ Baidu-Web-Api/src/main/java/pp/weiba/thirdparty/baidu/web/api/security/authentication/AuthenticationApiClient.java
@@ -1,12 +1,17 @@
 package pp.weiba.thirdparty.baidu.web.api.security.authentication;
 
+import cn.hutool.core.net.url.UrlBuilder;
+import cn.hutool.core.net.url.UrlQuery;
+import cn.hutool.core.util.StrUtil;
 import lombok.extern.log4j.Log4j2;
 import pp.weiba.framework.core.client.*;
 import pp.weiba.framework.core.convert.TypeReference;
 import pp.weiba.thirdparty.baidu.web.api.netdisk.UrlConstants;
+import pp.weiba.thirdparty.baidu.web.api.netdisk.XpanUrlConstants;
 import pp.weiba.thirdparty.baidu.web.api.netdisk.response.LoginStatusResponse;
 import pp.weiba.thirdparty.baidu.web.api.netdisk.response.TemplateVariableResponse;
 
+import java.util.Date;
 import java.util.HashMap;
 
 /**
@@ -68,5 +73,37 @@ public class AuthenticationApiClient extends AbstractApiHttpClient {
         if (httpResponse.getStatusCode() == 302) {
             log.info("退出登录成功，authenticationId:{}", authenticationId);
         }
+    }
+
+    public AccessToken getAccessToken() {
+        // 必须扫码或短信等方式登录拿到完整Cookie，再获取，否则会获取失败，因为它中间会跳转oath。
+        HttpRequest request = HttpRequest.urlFormatBuilder(Method.GET, XpanUrlConstants.GET_ACCESS_TOKEN)
+                .setFollowRedirect(true)
+                .setFollowRedirectsCookie(true)
+                .setMaxRedirectCount(2);
+        HttpResponse httpResponse = executeResponse(request);
+        if (httpResponse.getStatusCode() == 302) {
+            String locationUrl = httpResponse.getHeaders().get("Location");
+            if (StrUtil.isNotBlank(locationUrl) && locationUrl.contains("access_token")) {
+                // [https://openapi.baidu.com/oauth/2.0/login_success#expires_in=2592000&access_token=123.967deccce2d8ddcf833a88f23f839882.YDtogn0r5gj81Shl17DK5pP5D-S7-QhShgPnZZD.UKaJFA&session_secret=&session_key=&scope=basic+netdisk]
+                log.debug("获取accessToken成功, locationUrl:{}", locationUrl);
+                if (locationUrl.startsWith("[") && locationUrl.endsWith("]")) {
+                    locationUrl = locationUrl.substring(1, locationUrl.length() - 1);
+                }
+                if (locationUrl.startsWith("https://openapi.baidu.com/oauth/2.0/login_success#")) {
+                    locationUrl = locationUrl.replace("#", "?");
+                }
+                UrlQuery query = UrlBuilder.ofHttp(locationUrl).getQuery();
+                String accessToken = String.valueOf(query.get("access_token"));
+                long expiresIn = Long.parseLong(String.valueOf(query.get("expires_in")));
+                String session_secret = String.valueOf(query.get("session_secret"));
+                String scope = String.valueOf(query.get("scope"));
+                return new AccessToken(new Date(expiresIn), accessToken, session_secret, scope);
+            }
+            log.error("获取accessToken失败, locationUrl异常：{}", locationUrl);
+        }
+        String msg = "获取accessToken失败, 账号登录状态异常, 请核实Cookies是否正常！";
+        log.error(msg);
+        throw new RuntimeException(msg);
     }
 }
